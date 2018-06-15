08019c5fffb6a6aabb333f29cb618756f53b0eb2
Edward Gonzales <edwgonzales.eg@gmail.com>
2018-06-15 10:03:31 -0500

    added code pipeline to automate services

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -7,25 +7,42 @@ def lambda_handler(event,context):
     s3 = boto3.resource('s3')
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:559835763141:DeployPortfolioTopic')
+
+    location = {
+        "bucketName":'edwardgonzalesjr.portfoliobuild',
+        "objectKey":'build.zip'
+    }
     try:
+        job = event.get("CodePipeline.job")
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+        print "Building Portfolio from Lambda" + str(location)
+
+
         portfolio_bucket = s3.Bucket('portfolio.edwardgonzalesjr.link')
-        build_bucket = s3.Bucket('edwardgonzalesjr.portfoliobuild')
+        build_bucket = s3.Bucket(location["bucketName"])
 
 
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('build.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
-         for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
 
         print 'Job Done'
         topic.publish(Subject="Portfolio Deployed", Message="Portfolio Deployed Successfuly")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
     except:
         topic.publish(Subject="Portfolio Deployed failed", Message="Portfolio Deployed Successfuly")
         raise
