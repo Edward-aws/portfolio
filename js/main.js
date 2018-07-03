2eb320c89fef8eb05fc22138a593fe06790fbe8a
Edward Gonzales <edwgonzales.eg@gmail.com>
2018-07-03 08:52:37 -0500

    Add tests for ExampleWork and ExmpleWork Bubble

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("loaded react-dom");
+const myWork = [
+  {
+  'title': "Ecommerce Website",
+  'image':{
+  'desc':"example screenshot of a project involving code",
+  'src':"images/example1.png",
+  'comment':""
+  }
+},
+{
+'title': "Portfolio with ReactJS using Lambda",
+'image':{
+'desc':"Serverless Portfolio",
+'src':"images/example2.png",
+'comment':""
+  }
+},
+{
+'title': "Serverless Projects",
+'image':{
+'desc':"example screenshot of a project involving cats",
+'src':"images/example3.png",
+'comment':""
+    }
+  }
+]
 
-console.log("Webpack works");
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
