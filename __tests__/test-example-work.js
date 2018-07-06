7734737df10c1e1b22274e85be0a961fea7cf4fa
Edward Gonzales <edwgonzales.eg@gmail.com>
2018-07-06 11:59:01 -0500

    Added React Components for the work bubble

--- __tests__/test-example-work.js
+++ __tests__/test-example-work.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import ExampleWork, {ExampleWorkBubble } from '../js/example-work';
+import ExampleWork, { ExampleWorkBubble } from '../js/example-work';
 
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -27,17 +27,27 @@ const myWork = [
 describe("ExampleWork Component", () => {
   let component = shallow(<ExampleWork work={myWork}/>);
 
-  it("Shoulde be a 'section' element", () => {
-    expect(component.type()).toEqual('section');
+  it("Shoulde be a 'span' element", () => {
+    expect(component.type()).toEqual('span');
   });
 
   it("Should contan as many childrens as there are work examples", () => {
     expect(component.find("ExampleWorkBubble").length).toEqual(myWork.length);
   });
+
+  it("Should allow the modal to open and close", () => {
+    component.instance().openModal();
+    expect(component.instance().state.modalOpen).toBe(true);
+    component.instance().closeModal();
+    expect(component.instance().state.modalOpen).toBe(false);
+  })
 });
 
 describe("ExampleWorkBubble component", () => {
-  let component = shallow(<ExampleWorkBubble example={myWork[1]}/>);
+  let mockOpenModalFn = jest.fn();
+
+  let component = shallow(<ExampleWorkBubble example={myWork[1]}
+    openModal={mockOpenModalFn}/>);
 
   let images = component.find("img");
 
@@ -48,4 +58,9 @@ describe("ExampleWorkBubble component", () => {
 it("Should have the image src set correctly", () => {
   expect(images.prop('src')).toEqual(myWork[1].image.src);
 });
+
+it("Should Call the openModal handler when clicked", () => {
+  component.find(".section__exampleWrapper").simulate('click');
+  expect(mockOpenModalFn).toHaveBeenCalled();
+})
 });
